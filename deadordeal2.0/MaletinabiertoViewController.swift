3cbf4f028ebdd5c1add33713cad2267a58f632e1
VictoriaReyes1803 <139575570+Durann23@users.noreply.github.com>
2024-04-26 20:26:04 -0600

    Mensaje descriptivo de tus cambios

--- deadordeal2.0/MaletinabiertoViewController.swift
+++ deadordeal2.0/MaletinabiertoViewController.swift
@@ -6,15 +6,45 @@
 //
 
 import UIKit
+import AVFoundation
 
 class MaletinabiertoViewController: UIViewController {
     @IBOutlet weak var lblCantidad: UILabel!
     
-    @IBOutlet weak var imgMujer: UIImageView!
+    @IBOutlet weak var imgMaletin: UIImageView!
     var cantidadMostrada: String?
-
+    
+    //variables vienen de Maletines
+    var maletinUsuario: (posicion: Int, cantidad: Int)?
+    var cantidadLlevada:Int = 0
+    var cantidadeselegidas = [Int]()
+    var contadorClicks = 1
+    var cantidadIndex:Int = 0
+    var cases = 0
+    var tagC = [Int]()
+    var audioPlayer: AVAudioPlayer!
+    var player = AVAudioPlayer()
+    
+    
         override func viewDidLoad() {
             super.viewDidLoad()
+            
+            let audioFilename = "Old victory sound roblox"
+            
+            if let audioURL = Bundle.main.url(forResource: audioFilename, withExtension: "mp3")
+            {
+                do {
+                    audioPlayer = try AVAudioPlayer(contentsOf: audioURL)
+                    audioPlayer.numberOfLoops = 0
+                    audioPlayer.play()
+                } catch {
+                    print("Error al inicializar el reproductor de audio: \(error.localizedDescription)")
+                }
+            } else {
+                print("Archivo de audio no encontrado")
+            }
+        
+            
             if let cantidad = cantidadMostrada {
                        lblCantidad.text = "$" + cantidad + ".00"
                 self.performSegue(withIdentifier: "Quitadinero", sender: cantidad)
@@ -26,15 +56,17 @@ class MaletinabiertoViewController: UIViewController {
             let h = 0.5 * w
             let x = (view.frame.width - w)/2
             let y = -h
-            imgMujer.frame = CGRect(x: x, y: y, width: w, height: h)
-            imgMujer.alpha = 0.0
-            imgMujer.transform = CGAffineTransform(scaleX: 0.1, y: 0.1)
+            lblCantidad.alpha = 0.0
+            lblCantidad.transform = CGAffineTransform(scaleX: 0.1, y: 0.1)
+            imgMaletin.frame = CGRect(x: x, y: y, width: w, height: h)
+            imgMaletin.alpha = 0.0
+            imgMaletin.transform = CGAffineTransform(scaleX: 0.1, y: 0.1)
         }
         
         override func viewDidAppear(_ animated: Bool) {
             super.viewDidAppear(animated)
             
-            UIView.animate(withDuration: 1.5,
+            UIView.animate(withDuration: 0.5,
                            delay: 0,
                            usingSpringWithDamping: 0.6,
                            initialSpringVelocity: 0.8,
@@ -42,9 +74,11 @@ class MaletinabiertoViewController: UIViewController {
                            animations: {
                 let centerX = self.view.frame.width / 2
                 let centerY = self.view.frame.height / 2
-                self.imgMujer.center = CGPoint(x: centerX, y: centerY)
-                self.imgMujer.alpha = 1.0
-                self.imgMujer.transform = CGAffineTransform.identity
+                self.imgMaletin.center = CGPoint(x: centerX, y: centerY)
+                self.lblCantidad.alpha = 1.0
+                self.lblCantidad.transform = CGAffineTransform.identity
+                self.imgMaletin.alpha = 1.0
+                self.imgMaletin.transform = CGAffineTransform.identity
             },
                            completion: { finished in
                 if finished {
@@ -57,7 +91,18 @@ class MaletinabiertoViewController: UIViewController {
         if segue.identifier == "Quitadinero" {
              let cant = segue.destination as! DineroViewController
                 cant.Dinero = cantidadMostrada
-                print ("hola")
+            cant.maletinUsuario = maletinUsuario
+            cant.cantidadeselegidas = cantidadeselegidas
+            cant.contadorClicks = contadorClicks
+            cant.tagC = tagC
+            cant.cases = cases
+                print ("Pasaste a Quitar Dinero")
         }
+        
     }
+    
+    @IBAction func regresar(_ sender: Any) {
+        self.dismiss(animated: true)
+    }
+    
 }
--- deadordeal2.0/MaletinesViewController.swift
+++ deadordeal2.0/MaletinesViewController.swift
@@ -6,28 +6,61 @@
 //
 
 import UIKit
-
+import AVFoundation
 class MaletinesViewController: UIViewController {
     
-    
+    var jugador =  Jugador.sharedData()
+    @IBOutlet var backView: UIView!
     @IBOutlet weak var lblTitulo: UILabel!
     @IBOutlet var btnMaletines: [UIButton]!
+    @IBOutlet weak var maletinarriba: UIView!
+    @IBOutlet weak var lblCases: UILabel!
+    let records = Record.sharedRecords()
+    @IBOutlet weak var titlee: UILabel!
+    
+    @IBOutlet weak var botondevolver: UIButton!
+    @IBOutlet weak var usuariomaletin: UILabel!
     var cantidadesMaletines = [1,5,10,25,50,100,300,500,1000,5000,10000,25000,50000,100000,300000,500000]
-    var indiceCantidades = [Int]()
-    var maletinUsuario: (posicion: Int, cantidad: Int)?
+    @IBOutlet weak var LblChoosecases: UILabel!
+    var Cantidadesshuffle = [Int]()
+    var maletinUsuario:(posicion: Int, cantidad: Int)?
     var cantidadLlevada:Int = 0
     var cantidadeselegidas = [Int]()
     var contadorClicks = 1
     var cantidadIndex:Int = 0
+    var cases = 5
+    let maletin = Maletin.sharedData()
+    var tagC = [Int]()
+    var audioPlayer: AVAudioPlayer!
+    var player = AVAudioPlayer()
     
-        override func viewDidLoad() {
+    override func viewDidLoad() {
         super.viewDidLoad()
         
+     
+        
         print(cantidadesMaletines)
-        indiceCantidades = cantidadesMaletines
-        indiceCantidades.shuffle()
-        print(indiceCantidades)
+        Cantidadesshuffle = cantidadesMaletines
+        Cantidadesshuffle.shuffle()
+        print(Cantidadesshuffle)
+        
+        if tagC.count >= 2 {
+            lblTitulo.text = "Play Game"
+            for button in btnMaletines {
+                if tagC.contains(button.tag) {
+                    button.isHidden = true
+                }
+            }
+        }
+        
+        
+        
+    }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+           
+           
     }
     @IBAction func maletinTapped(_ sender: UIButton) {
         guard let index = btnMaletines.firstIndex(of: sender)
@@ -35,50 +68,265 @@ class MaletinesViewController: UIViewController {
         else {
             return
         }
-       print(index)
+        print(index)
         let tag = sender.tag
+        tagC.append(tag)
         print (tag)
         
-        cantidadIndex = indiceCantidades[tag]
+        cantidadIndex = Cantidadesshuffle[tag]
         print("Maletin",cantidadIndex)
+        maletin.guardarCantidadIndex(cantidadIndex)
         print("Tag del botón clickeado: \(tag)")
         
-     
+        
         if contadorClicks == 1 {
             maletinUsuario = (posicion: tag, cantidad: cantidadIndex)
-            contadorClicks += 1
+            contadorClicks = contadorClicks + 1
             lblTitulo.text = "Play Game"
+           
+            
         }
         else if contadorClicks < 6 && contadorClicks > 1 {
+            LblChoosecases.isHidden = false
+            cases -= 1
+            LblChoosecases.text = "Choose " + String(cases) + "  briefcases"
+            print("Choose \(String(cases))  briefcases")
+            
             print(cantidadIndex)
-                   cantidadLlevada += cantidadIndex
+            titlee.isHidden = true
+            cantidadLlevada += cantidadIndex
             cantidadeselegidas.append(cantidadIndex)
-            print(cantidadeselegidas)
+            contadorClicks = contadorClicks + 1
+            botondevolver.setTitle("Aceptar", for: .normal)
+           
+            LblChoosecases.isHidden = true
+            usuariomaletin.text = "$" + String(cantidadIndex)
+                maletinarriba.isHidden = false
             
-            
-            self.performSegue(withIdentifier: "MuestraDinero", sender: cantidadIndex)
-            
-               }
-        else {
-            self.performSegue(withIdentifier: "dealornodeal", sender: nil)
         }
-               sender.isHidden = true
+        else if contadorClicks == 6 || contadorClicks == 11
+        {
+            cantidadLlevada += cantidadIndex
+            cantidadeselegidas.append(cantidadIndex)
+            contadorClicks = contadorClicks + 1
+            if contadorClicks == 6 {
+                cases = 5
+            } else if contadorClicks == 11
+            {
+                cases = 4
+            }
+            LblChoosecases.isHidden = true
+            self.performSegue(withIdentifier: "dealornodeal", sender: cantidadeselegidas)
+            //siguio jugando
+        
+        } else if contadorClicks > 6 && contadorClicks < 11
+        
+        {   lblTitulo.text = "Ronda 2"
+            
+            LblChoosecases.text = "Choose " + String(cases) + "  briefcases"
+            LblChoosecases.isHidden = false
+            print("Choose \(String(cases))  briefcases")
+            cases -= 1
+            print(cantidadIndex)
+            cantidadLlevada += cantidadIndex
+            cantidadeselegidas.append(cantidadIndex)
+            contadorClicks = contadorClicks + 1
+            botondevolver.setTitle("aceptar", for: .normal)
+            titlee.isHidden = true
+            LblChoosecases.isHidden = true
+            usuariomaletin.text = "$" + String(cantidadIndex)
+                maletinarriba.isHidden = false
+           
+        
+            
+        } else if contadorClicks > 11 && contadorClicks <= 17
+        {
+            lblTitulo.text = "Ronda 3"
+                LblChoosecases.isHidden = false
+                LblChoosecases.text = "Choose " + String(cases) + "  briefcases"
+                
+                cases -= 1
+               
+                cantidadLlevada += cantidadIndex
+                cantidadeselegidas.append(cantidadIndex)
+                contadorClicks = contadorClicks + 1
+            botondevolver.setTitle("aceptar", for: .normal)
+            titlee.isHidden = true
+            LblChoosecases.isHidden = true
+            usuariomaletin.text = "$" + String(cantidadIndex)
+                maletinarriba.isHidden = false
+            if contadorClicks == 17
+                        
+            { lblTitulo.text = "ULTIMOOO MALETINN"
+                LblChoosecases.isHidden = false
+                LblChoosecases.text = "Choose " + String(cases) + "  briefcases"
+                cases -= 1
+                cantidadLlevada += cantidadIndex
+                cantidadeselegidas.append(cantidadIndex)
+                contadorClicks = contadorClicks + 1
+                botondevolver.isHidden = true
+                titlee.isHidden = true
+                LblChoosecases.isHidden = true
+                usuariomaletin.text = "$" + String(cantidadIndex)
+                maletinarriba.isHidden = false
+
+                // Oculta la vista actual después de un tiempo
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) { // Cambia el valor de 2.0 a la cantidad de segundos que desees
+                    // Muestra la siguiente vista
+                    self.lblTitulo.text = "Llegaste a tu maletin"
+                    
+                    self.LblChoosecases.isHidden = true
+                    if let cantidadMaletin = self.maletinUsuario?.cantidad {
+                        self.usuariomaletin.text = "$" + String(cantidadMaletin)
+                        self.jugador.guardarPuntos(cantidadMaletin)
+                        if cantidadMaletin <= 1000
+                        { self.titlee.text = "Lástima =("
+                            let audioFilename = "no-luck-too-bad-disappointing-sound-effect-112943"
+                            
+                            if let audioURL = Bundle.main.url(forResource: audioFilename, withExtension: "mp3")
+                            {
+                                do {
+                                    self.audioPlayer = try AVAudioPlayer(contentsOf: audioURL)
+                                    self.audioPlayer.numberOfLoops = 0
+                                    self.audioPlayer.play()
+                                } catch {
+                                    print("Error al inicializar el reproductor de audio: \(error.localizedDescription)")
+                                }
+                            } else {
+                                print("Archivo de audio no encontrado")
+                            }                        }
+                        else
+                        {
+                            self.titlee.text = "¡Felicidades!"
+                            let audioFilename = "Old victory sound roblox"
+                            
+                            if let audioURL = Bundle.main.url(forResource: audioFilename, withExtension: "mp3")
+                            {
+                                do {
+                                    self.audioPlayer = try AVAudioPlayer(contentsOf: audioURL)
+                                    self.audioPlayer.numberOfLoops = 0
+                                    self.audioPlayer.play()
+                                } catch {
+                                    print("Error al inicializar el reproductor de audio: \(error.localizedDescription)")
+                                }
+                            } else {
+                                print("Archivo de audio no encontrado")
+                            }
+                        
+                                    self.records.puntos.append(self.jugador.puntos)
+                                    self.records.nombres.append(self.jugador.nombre)
+                                    
+                                    // Verificar si el jugador ha ingresado al top 5
+                                    for i in 0..<self.records.puntos.count {
+                                        if self.jugador.puntos >= self.records.puntos[i] {
+                                            self.records.puntos.insert(self.jugador.puntos, at: i)
+                                            self.records.nombres.insert(self.jugador.nombre, at: i)
+                                            let alerta = UIAlertController(title: "¡Felicidades!", message: "Has entrado al top 5", preferredStyle: .alert)
+                                            let ok = UIAlertAction(title: "Aceptar", style: .default) { _ in
+                                                self.regresar()
+                                            }
+                                            alerta.addAction(ok)
+                                            self.present(alerta, animated: true)
+                                            break
+                                        }
+                                    }
+                            self.records.save()
+                            }
+                    } else {
+                        self.usuariomaletin.text = "Cantidad no disponible"
+                    }
+                    self.botondevolver.isHidden = false
+                    // Realiza la animación de entrada de maletinarriba
+                    self.maletinarriba.isHidden = false
+
+                    // Limpia las variables y ajustes para la siguiente ronda
+                    self.Cantidadesshuffle = [Int]()
+                    self.maletinUsuario = nil
+                    self.cantidadLlevada = 0
+                    self.cantidadeselegidas = [Int]()
+                    self.cantidadIndex = 0
+                    self.cases = 5
+                }
+
+            }
+            
+           
+        }
+        
+            
+            
+            
+        sender.isHidden = true
         
         if let maletin = maletinUsuario {
-               print("Cantidad maletin: \(maletin.posicion), \(maletin.cantidad)")
-           } else {
-               print("Maletin no seleccionado")
-           }
-               print("cantidad llevada",cantidadLlevada)
-            
-           }
+            print("Cantidad maletin: \(maletin.posicion), \(maletin.cantidad)")
+        } else
+        {
+            print("Maletin no seleccionado")
+        }
+        print("cantidad llevada",cantidadLlevada)
+        print("Hola ver cuantos click a echo",contadorClicks)
+        print("Cantidades Elegidas",cantidadeselegidas)
+        print("Tag clickeados", tagC)
+        
+    }
    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "MuestraDinero" {
-             let cant = segue.destination as! MaletinabiertoViewController
-            cant.cantidadMostrada = String(self.cantidadIndex)
-                print ("hola")
+    
+    
+    @IBAction func botondevolver(_ sender: Any) {
+        if contadorClicks < 18 {
+            maletinarriba.isHidden = true
+            
+            
+        
+        } else if contadorClicks == 18 {
+            
+         
+            contadorClicks = 1
+            tagC = [Int]()
+            let storyboard  = UIStoryboard(name: "Main", bundle: nil)
+            let menu = storyboard.instantiateViewController(withIdentifier: "menu")
+            menu.modalTransitionStyle = .crossDissolve
+            menu.modalPresentationStyle = .fullScreen
+            self.present(menu, animated: true)
         }
     }
-      }
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "MuestraDinero" {
+            let cant = segue.destination as! DineroViewController
+            cant.cantidadIndex = self.cantidadIndex
+            cant.maletinUsuario = maletinUsuario
+            cant.cantidadeselegidas = cantidadeselegidas
+            cant.contadorClicks = contadorClicks
+            cant.tagC = tagC
+            cant.cases = cases
+            
+            print ("Valores pasados a Muestra Dinero")
+        }
+        else if segue.identifier == "dealornodeal"
+        {
+            let deal = segue.destination as! BankerViewController
+            deal.cantidadMostrada = String(self.cantidadIndex)
+            deal.maletinUsuario = maletinUsuario
+            deal.cantidadeselegidas = cantidadeselegidas
+            deal.contadorClicks = contadorClicks
+            deal.tagC = tagC
+            deal.cases = cases
+            deal.maletin = maletinUsuario!.cantidad
+            
+            print ("Valores pasados a Deal or No Deal")
+        }
+        
+    }
+    func regresar()
+    {
+        let storyboard  = UIStoryboard(name: "Main", bundle: nil)
+        let menu = storyboard.instantiateViewController(withIdentifier: "menu")
+        menu.modalTransitionStyle = .crossDissolve
+        menu.modalPresentationStyle = .fullScreen
+        self.present(menu, animated: true)
+    }
+    
+    
+    
+}
